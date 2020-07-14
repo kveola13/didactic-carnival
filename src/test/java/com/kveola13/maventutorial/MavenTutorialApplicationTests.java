c0d1e4a9422e17cbdae0d17bcfe682f49d4e6dbb
olakv <kveola13@student.westerdals.no>
2020-07-15 01:27:16 +0200

    Init maven commit

---
+++ src/test/java/com/kveola13/maventutorial/MavenTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kveola13.maventutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
