cddad6fe2e432db4dc502426b7bb762161f56636
olakv <kveola13@student.westerdals.no>
2020-07-15 01:27:26 +0200

    Init maven commit

---
+++ src/main/java/com/kveola13/maventutorial/MavenTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.kveola13.maventutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenTutorialApplication.class, args);
+	}
+
+}
